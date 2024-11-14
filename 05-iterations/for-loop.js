00eaeaf84936b77ec8f2d4e9e3c6d957ab6b4044
Sakinazahra5 <sakinaz162@gmail.com>
2024-11-14 20:34:29 +0500

    for-loop

--- 05-iterations/for-loop.js
+++ 05-iterations/for-loop.js
@@ -6,7 +6,33 @@
 // }
 
 
-for (let i = 0; i = 10; i++) {
+for (let i = 0; i <= 10; i++) {
     const element = i;
+    if (element==5) {
+     //   console.log("5 is best number");
+        
+    }
+    //console.log(element);
+    
+}
+
+for (let i = 5; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+
+    for (let j = 5; j <= 10; j++) {
+        //console.log(`Inner loop[ value: ${j}] and inner loop ${i}`);
+    //console.log(i + '*' + j + '=' + i*j);
+
+    }
+}
+
+let myArray = ["tom", "jerry", "spike"]
+
+for (let index = 0; index < myArray.length) {
+    const element = myArray[index];
     console.log(element);
 }
+
+
+
+
