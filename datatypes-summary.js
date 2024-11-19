9bba0ce10f5e614f72b13252fcdb900f3093266c
Sakinazahra5 <sakinaz162@gmail.com>
2024-11-19 23:08:05 +0500

    project 4 solution code

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -30,3 +30,78 @@ const myFunction = function(){
 //************************************
 
 // Stack(Primitive), Heap(Non-Primitive)
+
+
+const randomNumber = parseInt(Math.random() * 100 + 1);
+const submit = document.querySelector(`#subt`);
+const userInput = document.querySelector(`#guessField`);
+const guessSlot = document.querySelector(`.guesses`);
+const remaining = document.querySelector(`.lastResult`);
+const lowOrHi = document.querySelector(`.lowOrHi`);
+const startOver = document.querySelector(`.resultParas`);
+const p = document.createElement(`p`);
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
+
+if (playGame) {
+  submit.addEventListener(`click`, function (e) {
+    e.preventDefault();
+    const guess = parseInt(userInput.value);
+    console.log(guess);
+    validateGuess(guess);
+  });
+}
+function validateGuess(guess) {
+  // Check if the guess is a number
+  if (isNaN(guess)) {
+    alert(`Please enter a valid number`);
+  } else if (guess < 1) {
+    // Fix the condition here
+    alert(`Please enter a number more than 1`);
+  } else if (guess > 100) {
+    // Fix the condition here
+    alert(`Please enter a  number less than 100`);
+  } else {
+    // Assuming `prevGuess` is an array and `numGuess` is the number of attempts
+    prevGuess.push(guess);
+    //how to check the number if guesses
+    if (numGuess === 11) {
+      displayGuess(guess);
+      displayMessage(`Game Over, Random number was ${randomNumber}`);
+      endGame();
+    } else {
+      displayGuess(guess);
+      checkGuess(guess);
+    }
+  }
+}
+function checkGuess(guess) {
+  if (guess === randomNumber) {
+    displayMessage(`You guessed it right`);
+    endGame();
+  } else if (guess < randomNumber) {
+    displayMessage(`Your guess is TOOO Low`);
+  } else if (guess > randomNumber) {
+    displayMessage(`Your guess is TOOO High`);
+  }
+  // clean-up guesses (previous)
+  function displayGuess(guess) {
+    userInput.value = '';
+    guessSlot.innerHTML += `${guess}`;
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
+  }
+  function displayMessage() {
+    lowOrHi.innerHTML = `<h2>${message}</h2>`;
+  }
+  function endGame() {
+    //
+  }
+  function newGame() {
+    //
+  }
+}
+
